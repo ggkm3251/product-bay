21124374073f3385877f446a30a882fd216d3502
glenn <glennkamau2@gmail.com>
2025-02-08 00:31:42 +0300

    fix: wrap the entire app with CartProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import './App.css'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Navbar from './components/NavBar';
+import ProductDetails from './pages/ProductDetails';
+import { CartProvider } from './components/CartContext';
+import CartPage from './pages/CartPage';
 
-function App() {
-  
-
+const App: React.FC = () => {
   return (
     <Router>
+      <CartProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </CartProvider>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
